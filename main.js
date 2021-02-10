99ec8c093a70b3eeedce8a1977a14e44ae3570b5
Chris Meffley <cmeffley@gmail.com>
2021-02-09 21:00:22 -0500

    package functions

--- main.js
+++ main.js
@@ -1,48 +1,5 @@
 console.log('CONNECTED!');
 
-// Get Package Form Info
-const packageForm = (e) => {
-  e.preventDefault();
-
-  const packageName = document.querySelector('#package-form').value;
-  const packageDescription = document.querySelector('#option-package').value;
-  
-const obj = {
-  packageName,
-  packageDescription,
-};
-
-users.push(obj);
-createPackage(users.packages);
-
-  document.querySelector('form-control').reset();
-};
-// Get Package Form Info
-
-// Create Package Card
-function createPackage() {
-  let domString = "";
-  users.forEach((element) => {
-    domString += `<div class="card border-dark mb-3" style="width: 25rem">
-    <div class="card-header">Package Name</div>
-    <div class="text">${element.packageName}</div>
-    <div class="card-body">
-      <p class="card-text">Package Description</p>
-      <div class="text">${element.packageDescription}</div>
-      <button type="button" class="btn btn-secondary">Delete</button>
-    </div>
-  </div>
-  <hr />`
-  })
-};
-
-const init = () => {
-  
-};
-
-init();
-
-// Create Package Card
 // *** Data Types *** //
 const users = [ 
   {
@@ -120,7 +77,46 @@ const printToDom = (divId, textToPrint) => {
 }
 
 // *** HTML Builder Functions *** //
+// Get Package Form Info
+const packageForm = (e) => {
+  e.preventDefault();
 
+  const packName = document.querySelector('#package-form').value;
+  const packDescription = document.querySelector('#option-package').value;
+  
+const obj = {
+  packName,
+  packDescription,
+};
+
+users.push(obj);
+createPackage(users.packages);
+
+  document.querySelector('#add-package').addEventListener('click', packageForm);
+  document.querySelector('form-control').reset();
+};
+// End Get Package Form Info
+
+// Create Package Card
+const createPackage = (taco) => {
+  let domString = "";
+  taco.forEach((element) => {
+    element.packages.forEach((arg) => {
+    domString += `<div class="card border-dark mb-3" style="width: 25rem">
+    <div class="card-header">Package Name</div>
+    <div class="text">${arg.packageName}</div>
+    <div class="card-body">
+      <p class="card-text">Package Description</p>
+      <div class="text">${arg.packageDescription}</div>
+      <button type="button" class="btn btn-secondary">Delete</button>
+    </div>
+  </div>
+  <hr />`
+      });
+      printToDom('#package-card', domString);
+  });
+
+};
 
 // ***  Event Handlers *** //
 
